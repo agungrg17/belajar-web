41bbc340916ff78601a194da33ee5df32f08ba4e
agungrg17 <agungrg17@gmail.com>
2021-08-02 18:43:34 +0700

    Commit all files

---
+++ index.js
@@ -0,0 +1,35 @@
+//alert
+$(document).ready(function () 
+    document.getElementById('exampleAlert').style.display='none';
+});
+function alert() {
+    document.getElementById('exampleAlert').style.display='block';
+}
+//rating
+jQuery(document).ready(function($){
+	    
+	$(".btnrating").on('click',(function(e) {
+	
+	var previous_value = $("#selected_rating").val();
+	
+	var selected_value = $(this).attr("data-attr");
+	$("#selected_rating").val(selected_value);
+	
+	$(".selected-rating").empty();
+	$(".selected-rating").html(selected_value);
+	
+	for (i = 1; i <= selected_value; ++i) {
+	$("#rating-star-"+i).toggleClass('btn-warning');
+	$("#rating-star-"+i).toggleClass('btn-default');
+	}
+	
+	for (ix = 1; ix <= previous_value; ++ix) {
+	$("#rating-star-"+ix).toggleClass('btn-warning');
+	$("#rating-star-"+ix).toggleClass('btn-default');
+	}
+	
+	}));
+	
+		
+});
+
